30edc4448f36a18b3ea43e5ff9a74b66248fe7b7
Kexick <trogister.to@gmail.com>
2025-09-05 02:22:39 +0600

    added exclude for ibl

--- home-manager/modules/nvf/modules/visual.nix
+++ home-manager/modules/nvf/modules/visual.nix
@@ -1,11 +1,20 @@
 {
   programs.nvf.settings.vim = {
     visuals = {
-      indent-blankline.enable = true;
-      # indent-blankline.setupOpts.indent.highlight = 
-      #       "IndentBlanklineIndent1"
-      # ;
+      indent-blankline = {
+        enable = true;
+        setupOpts = {
+          scope = {
+            enabled = true;
+            highlight = "IblScope";
+          };
+          exclude = {
+            filetypes = [ "dashboard" "alpha" "lazy" "help" "neo-tree" ];
+            buftypes  = [ "terminal" "nofile" ];
+          };
+        };
+      };
       nvim-web-devicons.enable = true;
+    };
   };
-     };
 }
