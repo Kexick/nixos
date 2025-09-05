bdc7d236e33be86e518000ae0d69567d5cac6779
Kexick <trogister.to@gmail.com>
2025-09-06 01:44:59 +0600

    added cmp-path

--- home-manager/modules/nvf/modules/autocomplete.nix
+++ home-manager/modules/nvf/modules/autocomplete.nix
@@ -1,9 +1,17 @@
+{ pkgs, ... }:
 {
   programs.nvf.settings.vim.autocomplete = {
     blink-cmp = {
       enable = true;
       friendly-snippets.enable = true;
       setupOpts.signature.enabled = true;
+      sourcePlugins = {
+        path = {
+          enable = true;
+          package = pkgs.vimPlugins.cmp-path;
+          module = "cmp_path";
+        };
+      };
     };
     enableSharedCmpSources = true;
   };
