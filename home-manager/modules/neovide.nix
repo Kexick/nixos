6a65b5e0a7f6d1952e07c413155bcede31bba027
Kexick <trogister.to@gmail.com>
2025-09-08 16:21:25 +0600

    added neovide options

---
+++ home-manager/modules/neovide.nix
@@ -0,0 +1,12 @@
+{
+  programs.neovide = {
+    enable = true;
+    settings = {
+      font = {
+        size = 14.0;
+        normal = "JetBrainsMono Nerd Font Mono";
+      };
+      box-drawning.mode = "native";
+    };
+  };
+}
