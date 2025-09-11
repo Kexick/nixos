fd1d4a86959a3278fdd82764add84a81edf3ed93
Kexick <trogister.to@gmail.com>
2025-09-11 08:49:28 +0600

    added a new hash

--- home-manager/modules/anime-launcher.nix
+++ home-manager/modules/anime-launcher.nix
@@ -2,7 +2,7 @@
   let
   aagl-gtk-on-nix = import (builtins.fetchTarball {
       url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-      sha256 = "0nyj3vvvapbr7x71s9f9rxkrvnq8kr4mcdid4x00v2397ia3jimg";
+      sha256 = "1pi18grynxv45kigrlmphkmq527hw85b7cgbz8mqvpw79ndj9yj5";
     } );
 in
 {
