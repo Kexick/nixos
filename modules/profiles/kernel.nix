c7caff6f0e75cc27f287a274a7d98ccbd76c48cb
Kexick <trogister.to@gmail.com>
2025-09-12 15:48:10 +0600

    added a kernel module

--- modules/profiles/kernel.nix
+++ modules/profiles/kernel.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   powerManagement.cpuFreqGovernor = "schedutil";
   boot.kernelPackages = pkgs.linuxPackages_lqx;
   boot = { 
-    
+    extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
     kernel.sysctl = { 
       "vm.vfs_cache_pressure" = 50; 
       "vm.swappiness" = 10;
