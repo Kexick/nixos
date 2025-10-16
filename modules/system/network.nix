b1c170cab04e2b015506c7d705b586273aa17b2c
Kexick <trogister.to@gmail.com>
2025-10-16 16:17:24 +0600

    refactor(config): hardware-configuration.nix -> hosts/desktop/hardware-configuration.nix

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,8 +1,10 @@
+{ lib, ... }:
 {
 	networking = {
 		hostName              = "nixos"; # Define your hostname.
 		networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 		nameservers           = [ "1.1.1.1" "8.8.8.8" "0.0.0.0"];
+        useDHCP = lib.mkDefault true;
 	};
 		# Configure network proxy if necessary
 		# networking.proxy.default = "http://user:password@proxy:port/";
