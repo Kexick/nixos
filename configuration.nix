c4bbccc830d98f54b052ab59c06a577af48be930
Kexick <trogister.to@gmail.com>
2025-10-16 15:54:05 +0600

    refactor(config): move stateVerion to hosts/desktop/default.nix

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,5 @@
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
   # Set your time zone.
   time.timeZone = "Asia/Omsk";
-
-
   i18n.defaultLocale = "ru_RU.UTF-8";
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  system.stateVersion = "25.05"; # Did you read the comment?
 }
