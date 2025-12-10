61db6318941ca71a6af6b6fd8996d205390ce4d4
Kexick <trogister.to@gmail.com>
2025-12-11 02:05:42 +0600

    chore(git): remove old account data

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,20 +1,9 @@
 {
   programs.git = {
     enable = true;
-    userName = "Kexick";
-    userEmail = "trogister.to@gmail.com";
-    includes = [
-      {
-        path = "~/.config/git/gitconfig-gitlab.inc";
-        condition = "gitdir:~/Project/21-school";
-        contents.user.name = "Flathead Alexis";
-        contents.user.mail = "flatheaa@student.21-school.ru";
-      }
-    ];
+    settings = {
+        user.name = "Kexick";
+        user.email = "trogister.to@gmail.com";
+    };
   };
-  xdg.configFile."git/gitconfig-gitlab.inc".text = ''
-    [user]
-      name = Flathead Alexis
-      email = flatheaa@student.21-school.ru
-  '';
 }
