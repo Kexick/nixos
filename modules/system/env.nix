e99facbec9609c7ad5512c493653a73b8126ef59
Kexick <trogister.to@gmail.com>
2025-12-11 02:01:26 +0600

    fix(env): fix electron apps

--- modules/system/env.nix
+++ modules/system/env.nix
@@ -5,10 +5,12 @@
       VISUAL = "nvim";
     };
     sessionVariables = {
-      NIXOS_OZONE_WL = "1";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      # SDL_VIDEODRIVER = "wayland";
-      # MOZ_ENABLE_WAYLAND = "1";
+        NIXOS_OZONE_WL = "1";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        VK_LAYER_WSI_WAYLAND_NO_DMABUF = "1";
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
+# SDL_VIDEODRIVER = "wayland";
+# MOZ_ENABLE_WAYLAND = "1";
     };
   };
 }
