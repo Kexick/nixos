5c826f855403d2ec90b8e27617b7b43acbb031f1
Kexick <trogister.to@gmail.com>
2025-12-11 02:02:33 +0600

    feat(audio): add alsa support32bit

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -4,6 +4,7 @@
     pipewire = {
       enable             = true;
       alsa.enable        = true;
+      alsa.support32Bit  = true;
       pulse.enable       = true;
       jack.enable        = true;
       wireplumber.enable = true;
