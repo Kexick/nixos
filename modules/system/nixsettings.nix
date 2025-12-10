8edd665effdd45e49dcdd68399515a8393726fb6
Kexick <trogister.to@gmail.com>
2025-12-11 01:56:42 +0600

    feat(nixsettings): add mirrors

--- modules/system/nixsettings.nix
+++ modules/system/nixsettings.nix
@@ -1,3 +1,17 @@
 {
-  nix.settings.trusted-users = [ "root" "kexick" ];
+  nix.settings = { 
+    cores                 = 0;
+    max-jobs              = 4;
+    http-connections      = 50;
+    max-substitution-jobs = 20;
+
+    trusted-users = [ "root" "kexick" ];
+
+    extra-substituters = [
+        "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
