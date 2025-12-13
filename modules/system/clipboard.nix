2dd8024a0c27ae13b928f646453d54a6a69ebca4
Kexick <trogister.to@gmail.com>
2025-12-14 02:40:47 +0600

    fix(clipboard): avoid hardcoding WAYLAND-DISPLAY

--- modules/system/clipboard.nix
+++ modules/system/clipboard.nix
@@ -1,27 +1,26 @@
 { pkgs, ... }:
 {
-  systemd.user.services.cliphist = {
+    systemd.user.services.walker = {
+        description = "Walker background service";
+        after = [ "graphical-session.target" ];
+        wantedBy = [ "graphical-session.target" ];
+        serviceConfig = {
+            ExecStart = "${pkgs.walker}/bin/walker --gapplication-service";
+            Restart = "always";
+            RestartSec = 1;
+        };
+    };
+
+    systemd.user.services.cliphist = {
     description   = "Clipboard history for Wayland";
     after         = [ "graphical-session.target" ];
     wantedBy      = [ "graphical-session.target" ];
     serviceConfig = {
-      Environment = "WAYLAND_DISPLAY=wayland-1";
-      ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store";
-      Restart   = "always";
+          ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store";
+          Restart   = "always";
+        };
     };
-  };
 
-  systemd.user.services.walker = {
-      description = "Walker background service";
-      after = [ "graphical-session.target" ];
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        Environment = ["WAYLAND_DISPLAY=wayland-1" "TERMINAL=foot"];
-        ExecStart = "walker --gapplication-service";
-        Restart = "always";
-        RestartSec = 1;
-      };
-    };
 
   environment.systemPackages = [
     pkgs.wl-clipboard
