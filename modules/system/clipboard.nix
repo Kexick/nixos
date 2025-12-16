6e332754e4981e0626c2109af8ad09e8ca9e7201
Kexick <trogister.to@gmail.com>
2025-12-16 18:02:59 +0600

    fix(clipboard): remove walker service

--- modules/system/clipboard.nix
+++ modules/system/clipboard.nix
@@ -1,29 +1,46 @@
 { pkgs, ... }:
+
 {
-    systemd.user.services.walker = {
-        description = "Walker background service";
-        after = [ "graphical-session.target" ];
-        wantedBy = [ "graphical-session.target" ];
-        serviceConfig = {
-            ExecStart = "${pkgs.walker}/bin/walker --gapplication-service";
-            Restart = "always";
-            RestartSec = 1;
-        };
+#   systemd.user.services.walker = {
+#     description = "Walker background service (Wayland only)";
+#     wantedBy = [ "default.target" ];
+#     path = [ 
+#         pkgs.bash
+#         pkgs.cliphist
+#         pkgs.coreutils
+#         pkgs.foot
+#         pkgs.swww 
+#         pkgs.wl-clipboard
+#     ];
+#     unitConfig = {
+#         StartLimitBurst = 3;
+#         StartLimitIntervalSec = 5;
+#     };
+#     serviceConfig = {
+#         ExecStartPre = "${pkgs.bash}/bin/bash -lc \"while ! ls \\\"$XDG_RUNTIME_DIR\\\"/wayland-* >/dev/null 2>&1; do sleep 1; done\"";
+#       ExecStart = "${pkgs.walker}/bin/walker --gapplication-service";
+#       Restart = "on-failure";
+#       RestartSec = 1;
+#     };
+#   };
+#
+  systemd.user.services.cliphist = {
+    description = "Clipboard history for Wayland";
+    wantedBy = [ "default.target" ];
+    unitConfig = {
+        StartLimitBurst = 3;
+        StartLimitIntervalSec = 5;
     };
-
-    systemd.user.services.cliphist = {
-    description   = "Clipboard history for Wayland";
-    after         = [ "graphical-session.target" ];
-    wantedBy      = [ "graphical-session.target" ];
     serviceConfig = {
-          ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store";
-          Restart   = "always";
-        };
+      ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store";
+      Restart = "on-failure";
+      RestartSec = 1;
+      SyslogIdentifier = "cliphist";
+      Environment = [ "G_MESSAGES_PREFIXED=all" ];
     };
-
-
-  environment.systemPackages = [
-    pkgs.wl-clipboard
-    pkgs.cliphist
-  ];
+  };
+     environment.systemPackages = [
+        pkgs.wl-clipboard
+        pkgs.cliphist
+      ];
 }
