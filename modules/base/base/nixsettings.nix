a702259a72c0f682f09c87c63fac752affff5801
Kexick <trogister.to@gmail.com>
2026-02-12 17:43:23 +0600

    refactor(nixos): reorganize system modules into base structure

---
+++ modules/base/base/nixsettings.nix
@@ -0,0 +1,21 @@
+{
+  nix.settings = {
+    cores = 0;
+    max-jobs = 4;
+    http-connections = 50;
+    max-substitution-jobs = 20;
+
+    trusted-users = ["root" "kexick"];
+
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
