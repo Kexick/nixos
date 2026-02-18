17b21ac4a86a67896c67a3858ce500e5820b509b
Kexick <trogister.to@gmail.com>
2026-02-18 21:47:41 +0600

    chore(home): update hash

--- home/modules/apps/anime-launcher.nix
+++ home/modules/apps/anime-launcher.nix
@@ -1,8 +1,10 @@
 let
-  aagl-gtk-on-nix = import (builtins.fetchTarball {
-    url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "137rfwkqyf0s05fvivl9z1n3dnwws5h7mybp76hgga8y6p0bxm2q";
-  });
+  aagl-gtk-on-nix =
+    import (builtins.fetchTarball {
+        url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
+        sha256 = "1zvj4a1qly9kpvawinrabyak3g70p02dkqbl8pdd2z4icc4jha5l
+";
+      });
 in {
   home.packages = [aagl-gtk-on-nix.an-anime-game-launcher];
 }
