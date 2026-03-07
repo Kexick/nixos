a73886c2b394b929140bdc1d2f8a8455a417aeac
Kexick <trogister.to@gmail.com>
2026-03-08 03:04:45 +0600

    enabled nvidia powerManagement and nvidiaSettings

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,18 +1,33 @@
-{ pkgs, ...}:
 {
-  environment.systemPackages = [ pkgs.nvidia_oc ];
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  # environment.systemPackages = [pkgs.nvidia_oc];
+  boot.kernelParams = ["nvidia.NVreg_EnableGpuFirmware=0" "pcie_port_pm=off"];
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_PreserveVideoMemoryAllocations=1
+  '';
   hardware = {
     graphics = {
-      enable      = true;
+      enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        libva-vdpau-driver
+        libvdpau-va-gl
+        nvidia-vaapi-driver
+      ];
     };
     nvidia = {
-      open                   = false;
-      nvidiaSettings         = true;
-      videoAcceleration      = true;
-      modesetting.enable     = true;
+      open = false;
+      nvidiaSettings = true;
+      videoAcceleration = true;
+      modesetting.enable = true;
       powerManagement.enable = true;
     };
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+
+  nixpkgs.config.nvidia.acceptLicense = true;
+  services.xserver.videoDrivers = ["nvidia"];
 }
