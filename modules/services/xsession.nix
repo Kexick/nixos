a9f5453c2bbf89c7f761a0691a637aca0662bf9e
Kexick <trogister.to@gmail.com>
2026-03-08 03:11:36 +0600

    chore(openbox): remove unnecessary settings
    - remove steam startup
    - remove videographical overclock

--- modules/services/xsession.nix
+++ modules/services/xsession.nix
@@ -1,36 +1,21 @@
 {pkgs, ...}: {
-  security.sudo.extraRules = [
-    {
-      users = ["kexick"];
-      commands = [
-        {
-          command = "${pkgs.nvidia_oc}/bin/nvidia_oc";
-          options = ["NOPASSWD"];
-        }
-      ];
-    }
-  ];
-  services.xserver.deviceSection = ''
-    Option "Coolbits" "28"
-  '';
+  # services.xserver.deviceSection = ''
+  #   Option "Coolbits" "28"
+  # '';
   services.xserver.displayManager.session = [
     {
       manage = "window";
-      name = "openbox-steam";
+      name = "openbox";
       start = ''
-          export __GLX_VENDOR_LIBRARY_NAME=nvidia
-          export XDG_SESSION_TYPE=x11
-          export GDK_BACKEND=x11
-          export STEAM_USE_OZONE=0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export XDG_SESSION_TYPE=x11
+        export GDK_BACKEND=x11
+        export STEAM_USE_OZONE=0
 
-          ${pkgs.nvidia_oc}/bin/nvidia_oc set --index 0 --freq-offset 120 --mem-offset 500
-
-          ${pkgs.openbox}/bin/openbox-session &
-        ( ${pkgs.steam}/bin/steam; ${pkgs.openbox}/bin/openbox --exit ) &
-          waitPID=$!
+        ${pkgs.openbox}/bin/openbox-session
       '';
     }
   ];
 
-  services.displayManager.defaultSession = "none+openbox-steam";
+  services.displayManager.defaultSession = "none+openbox";
 }
