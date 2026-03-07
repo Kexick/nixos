f3e344ed2eb79edd3b63ff927e0c197062d4e2ed
Kexick <trogister.to@gmail.com>
2026-03-08 03:23:58 +0600

    update hash

--- home/modules/apps/anime-launcher.nix
+++ home/modules/apps/anime-launcher.nix
@@ -1,7 +1,7 @@
 let
   aagl-gtk-on-nix = import (builtins.fetchTarball {
     url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "1zvj4a1qly9kpvawinrabyak3g70p02dkqbl8pdd2z4icc4jha5l";
+    sha256 = "19d1a4x3kcdzi0w8bzll8ws3hpnz3nrxdknc8vsrpyngbp3ws8ik";
   });
 in {
   home.packages = [aagl-gtk-on-nix.an-anime-game-launcher];
