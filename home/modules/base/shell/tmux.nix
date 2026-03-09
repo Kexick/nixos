43b7effebe9180f81e6521ce9cb202c1898384cb
Kexick <trogister.to@gmail.com>
2026-03-09 15:47:46 +0600

    style(tmux): add rounded in status bar

--- home/modules/base/shell/tmux.nix
+++ home/modules/base/shell/tmux.nix
@@ -19,8 +19,8 @@
     extraConfig = ''
 
       # Split window Alt + h/v
-      bind -n M-h split-window -h -c "#{pane_current_path}"
-      bind -n M-v split-window -v -c "#{pane_current_path}"
+      bind -n M-v split-window -h -c "#{pane_current_path}"
+      bind -n M-h split-window -v -c "#{pane_current_path}"
       unbind '"'
       unbind %
 
@@ -58,21 +58,14 @@
       # Status bar
       set -g status-position bottom
       set -g status-style bg=colour234,fg=colour137
-      set -g status-left '#[fg=colour233,bg=colour241] #S '
-      set -g status-right '#[fg=colour233,bg=colour241] %H:%M '
-      set -g window-status-current-format "#[fg=#77dd77][]#[fg=#1e1e2e,bg=#77dd77] #I #W #[fg=#77dd77,bg=default][]"
-      set -g window-status-format "#[fg=#a3b7a0][]#[fg=#1e1e2e,bg=#a3b7a0] #I #W #[fg=#a3b7a0,bg=default][]"
+      set -g status-left '#[fg=#1e1e2e,bg=colour241]  #S #[fg=colour241,bg=colour234] '
+      set -g status-right '#[fg=colour241,bg=colour234]#[fg=colour233,bg=colour241]  %H:%M '
+      set -g window-status-current-format "#[fg=#77dd77]#[fg=#1e1e2e,bg=#77dd77] #I #W #[fg=#77dd77,bg=default]"
+      set -g window-status-format "#[fg=#a3b7a0]#[fg=#1e1e2e,bg=#a3b7a0] #I #W #[fg=#a3b7a0,bg=default]"
+      set -g window-status-separator " "
 
       set -g pane-border-lines simple
 
-      green="#77dd77"
-      light_green="#a3b7a0"
-
-      set -g window-status-current-format "#[fg=#77dd77]#[fg=#1e1e2e,bg=#77dd77] #I #W #[fg=#77dd77,bg=default]"
-
-      set -g window-status-format "#[fg=#a3b7a0]#[fg=#1e1e2e,bg=#a3b7a0] #I #W #[fg=#a3b7a0,bg=default]"
-
-      set -g window-status-separator " "
     '';
 
     plugins = with pkgs.tmuxPlugins; [
