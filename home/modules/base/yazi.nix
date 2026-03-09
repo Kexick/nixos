781f096144d02b7fdaa5ac6b149d76c986965119
Kexick <trogister.to@gmail.com>
2026-03-09 15:47:06 +0600

    feat(home): configure yuzi

--- home/modules/base/yazi.nix
+++ home/modules/base/yazi.nix
@@ -6,10 +6,16 @@
         enabled = false;
       };
       mgr = {
+        ratio = [0 3 5];
         show_hidden = false;
         sort_by = "mtime";
         sort_dir_first = true;
         sort_reverse = true;
+        scrolloff = 7;
+      };
+      preview = {
+        image_protocol = "kitty";
+        image_filter = "nearest";
       };
     };
   };
