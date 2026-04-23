52d64ba9f0c9c424929a33f489eefcceb20ac23f
Kexick <trogister.to@gmail.com>
2026-04-24 05:36:52 +0600

    feat(core): restrict syncthing ports to LAN via nftables
    Allow Syncthing traffic only from 192.168.0.0/24 on enp42s0,
    drop external connections on port 22000.

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -4,6 +4,7 @@
     hostName = "nixos"; # Define your hostname.
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
     nameservers = ["1.1.1.1" "8.8.8.8"];
+
     firewall = {
       allowedTCPPortRanges = [
         {
@@ -15,6 +16,7 @@
           to = 5100;
         }
       ];
+
       allowedUDPPortRanges = [
         {
           from = 1714;
@@ -25,10 +27,15 @@
           to = 46796;
         }
       ];
+
+      extraInputRules = ''
+        iifname "enp42s0" ip saddr 192.168.0.0/24 tcp dport 22000 accept
+        iifname "enp42s0" ip saddr 192.168.0.0/24 udp dport 22000 accept
+
+        tcp dport 22000 drop
+        udp dport 22000 drop
+      '';
     };
   };
   services.resolved.enable = true;
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 }
