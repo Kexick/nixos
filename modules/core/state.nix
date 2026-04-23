a16bd7e1026d3f796292ae817df7e4e9986fe8df
Kexick <trogister.to@gmail.com>
2026-04-24 05:37:28 +0600

    feat(core): add cyrillic console font and keyboard layout
    Set terminus font for TTY to support cyrillic rendering,
    add ruwin_cplk keymap for language switching.

--- modules/core/state.nix
+++ modules/core/state.nix
@@ -1,8 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   # Set your time zone.
   time.timeZone = "Asia/Omsk";
 
   i18n.defaultLocale = "ru_RU.UTF-8";
+  console = {
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v24n.psf.gz";
+    keyMap = "ruwin_cplk-UTF-8";
+    packages = [pkgs.terminus_font];
+  };
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
