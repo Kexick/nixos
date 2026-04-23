bbe47f6137357d906c1494aabb6ae3161735c805
Kexick <trogister.to@gmail.com>
2026-04-24 05:53:32 +0600

    fix(home/dev): fix box-drawing typo in neovide config

--- home/modules/dev/neovide.nix
+++ home/modules/dev/neovide.nix
@@ -9,7 +9,7 @@
           size = 14.0;
           normal.family = "JetBrainsMonoNL Nerd Font Mono";
         };
-        box-drawning.mode = "native";
+        box-drawing.mode = "native";
       };
     };
   };
