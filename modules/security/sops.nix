fdc1adc10cb451d495a6b42bd2f9eb8ac9287991
Kexick <trogister.to@gmail.com>
2026-04-24 05:35:41 +0600

    feat(security): add sops-nix secrets management and xray proxy

---
+++ modules/security/sops.nix
@@ -0,0 +1,27 @@
+{...}: let
+  xray-common = {
+    sopsFile = ../../secrets/xray.yaml;
+    owner = "root";
+    group = "root";
+    mode = "0400";
+  };
+in {
+  sops = {
+    validateSopsFiles = true;
+    age.keyFile = "/var/lib/sops-nix/key.txt";
+    secrets = {
+      "xray/id" = xray-common;
+      "xray/shortId" = xray-common;
+      "xray/mldsa65Verify" = xray-common;
+      "xray/spiderX" = xray-common;
+      "xray/encryption" = xray-common;
+      "xray/address" = xray-common;
+      "env/custom_api_key" = {
+        sopsFile = ../../secrets/env.yaml;
+        owner = "kexick";
+        group = "users";
+        mode = "0400";
+      };
+    };
+  };
+}
--- modules/services/xray.nix
+++ modules/services/xray.nix
@@ -1,6 +1,136 @@
-{
+{config, ...}: let
+  s = config.sops.placeholder;
+in {
   services.xray = {
     enable = true;
-    settingsFile = "/home/kexick/.config/xray/new.json";
+    settingsFile = config.sops.templates."config.json".path;
+  };
+
+  sops.templates."config.json" = {
+    owner = "root";
+    group = "root";
+    mode = "0400";
+
+    content = ''
+      {
+        "dns": {
+          "queryStrategy": "UseIP",
+          "servers": [
+            {
+            "address": "8.8.8.8",
+            "skipFallback": false
+            }
+          ],
+          "tag": "dns_out"
+        },
+        "inbounds": [
+          {
+            "port": 1083,
+            "listen": "127.0.0.1",
+            "protocol": "mixed",
+            "settings": {
+              "auth": "noauth",
+              "udp": true,
+              "userLevel": 8 },
+            "sniffing": {
+              "destOverride": [
+                "http",
+                "tls",
+                "quic",
+                "fakedns"
+              ],
+              "enabled": true
+            },
+            "tag": "mixed"
+          },
+          {
+            "port": 1084,
+            "listen": "127.0.0.1",
+            "protocol": "http",
+            "settings": {
+              "userLevel": 8
+            },
+            "tag": "http"
+          }
+        ],
+        "log": {
+          "loglevel": "warning"
+        },
+        "outbounds": [
+          {
+            "protocol": "vless",
+            "tag": "proxy",
+            "streamSettings": {
+              "network": "tcp",
+              "security": "reality",
+              "realitySettings": {
+                "fingerprint": "chrome",
+                "mldsa65Verify": "${s."xray/mldsa65Verify"}",
+                "publicKey": "K9WETldjTK1qtt1l06wll4YPbjgWtAs7Lm7wuaV9bnI",
+                "serverName": "max.ru",
+                "shortId": "${s."xray/shortId"}",
+                "show": false,
+                "spiderX": "${s."xray/spiderX"}"
+              },
+              "tcpSettings": {
+                "header": {
+                "type": "none"
+                }
+              }
+            },
+            "settings": {
+              "address": "${s."xray/address"}",
+              "encryption": "${s."xray/encryption"}",
+              "id": "${s."xray/id"}",
+              "port": 10271
+            }
+          },
+          {
+          "protocol": "freedom",
+            "settings": {
+              "domainStrategy": "AsIs",
+              "noises": [],
+              "redirect": ""
+            },
+          "tag": "direct"
+          },
+          {
+          "protocol": "blackhole",
+            "settings": {
+              "response": {
+              "type": "http"
+              }
+          },
+            "tag": "block"
+          }
+        ],
+        "policy": {
+          "levels": {
+            "8": {
+            "connIdle": 300,
+            "downlinkOnly": 1,
+            "handshake": 4,
+            "uplinkOnly": 1
+            }
+          },
+          "system": {
+            "statsOutboundDownlink": true,
+            "statsOutboundUplink": true
+          }
+        },
+        "remarks": "Финка Reallity",
+        "routing": {
+          "domainStrategy": "AsIs",
+          "rules": [
+            {
+              "network": "tcp,udp",
+              "outboundTag": "proxy",
+              "type": "field"
+            }
+          ]
+        },
+        "stats": {}
+      }
+    '';
   };
 }
