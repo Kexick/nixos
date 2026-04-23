840f8ca3001e8374167de3f728e8efead4de7664
Kexick <trogister.to@gmail.com>
2026-04-24 05:37:50 +0600

    feat(core): add i2c group for DDC monitor control

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   users.users.kexick = {
     isNormalUser = true;
-    extraGroups = ["wheel" "input" "render" "video"];
+    extraGroups = ["wheel" "input" "render" "video" "i2c"];
     shell = pkgs.zsh;
     linger = true;
   };
