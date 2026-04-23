921ff3e54e6259bfe8261f6f4eac72e982effea8
Kexick <trogister.to@gmail.com>
2026-04-24 05:36:33 +0600

    feat(core): add security-related sysctl parameters
    Enable unprivileged user namespaces and set loose reverse path
    filtering for compatibility with multi-homed network setup.

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -10,6 +10,9 @@
     kernel.sysctl = {
       "vm.vfs_cache_pressure" = 50;
       "vm.swappiness" = 10;
+      "kernel.unprivileged_userns_clone" = 1;
+      "net.ipv4.conf.all.rp_filter" = 2;
+      "net.ipv4.conf.default.rp_filter" = 2;
     };
   };
 }
