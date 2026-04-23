818fa894a3f6f645cb0cacf71946094862eefbc3
Kexick <trogister.to@gmail.com>
2026-04-24 05:45:06 +0600

    feat(services): add nvidia and proton env variables to openbox session
    Set vsync, DXVK async and esync/fsync flags for gaming under X11.

--- modules/services/xsession.nix
+++ modules/services/xsession.nix
@@ -12,6 +12,15 @@
         export GDK_BACKEND=x11
         export STEAM_USE_OZONE=0
 
+        export __GL_SYNC_TO_VBLANK=0
+        export __GL_MaxFramesAllowed=1
+
+        export __NV_PRIME_RENDER_OFFLOAD=0
+        export PROTON_USE_WINED3D=0
+        export DXVK_ASYNC=1
+
+        export PROTON_NO_ESYNC=0
+        export PROTON_NO_FSYNC=0
         ${pkgs.openbox}/bin/openbox-session
       '';
     }
