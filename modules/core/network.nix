a2e95dd750edb3036ece747c4b974ceda5bde827
Kexick <trogister.to@gmail.com>
2026-04-24 08:32:27 +0600

    feat(core): switch to nftables, restrict firewall to LAN-only syncthing
    Replace wide open port ranges (TCP 443-5100, UDP 19364-46796) with
    nftables rules allowing only Syncthing port 22000 from 192.168.0.0/24.

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -4,34 +4,12 @@
     hostName = "nixos"; # Define your hostname.
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
     nameservers = ["1.1.1.1" "8.8.8.8"];
+    nftables.enable = true;
 
     firewall = {
-      allowedTCPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-        {
-          from = 443;
-          to = 5100;
-        }
-      ];
-
-      allowedUDPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-        {
-          from = 19364;
-          to = 46796;
-        }
-      ];
-
       extraInputRules = ''
         iifname "enp42s0" ip saddr 192.168.0.0/24 tcp dport 22000 accept
         iifname "enp42s0" ip saddr 192.168.0.0/24 udp dport 22000 accept
-
         tcp dport 22000 drop
         udp dport 22000 drop
       '';
