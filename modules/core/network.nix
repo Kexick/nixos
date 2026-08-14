fc2ff01d62686639c9e4da85b2b324222a9dcd53
Kexick <trogister.to@gmail.com>
2026-08-14 18:00:02 +0600

    feat(network): configure systemd-resolved - enable systemd-resolved for NetworkManager - add public DNS servers

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,10 +2,20 @@
   environment.systemPackages = [pkgs.proxychains-ng];
   networking = {
     hostName = "nixos";
-    networkmanager.enable = true;
-    nameservers = ["1.1.1.1" "8.8.8.8"];
+    networkmanager = 
+      {
+        enable = true; 
+        dns = "systemd-resolved";
+      };
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+      "9.9.9.9"
+      "94.140.14.14"
+    ];
     nftables.enable = true;
-
     firewall = {
       extraInputRules = ''
         iifname "enp42s0" ip saddr 192.168.0.0/24 tcp dport 22000 accept
@@ -15,8 +25,11 @@
       '';
     };
   };
-  services.resolved.settings.Resolve = {
+  services.resolved = {
     enable = true;
-    llmnr = "false";
+      settings.Resolve = {
+        enable = true;
+        llmnr = "false";
+      };
   };
 }
