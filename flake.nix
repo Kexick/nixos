e56b85fa2344712c298ac70aeba08daa51decd63
Kexick <trogister.to@gmail.com>
2026-09-20 23:59:24 +0600

    fix: hyprland input

--- flake.nix
+++ flake.nix
@@ -19,11 +19,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-   hyprland.url = "github:hyprwm/Hyprland";
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland/tags/v0.56.2";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # hyprland-plugins = {
     #   url = "github:hyprwm/hyprland-plugins";
     #   inputs.hyprland.follows = "hyprland";
